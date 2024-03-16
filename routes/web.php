50d20cfc4149c25ad636cbbb4af74493a49bb5b2
rudra47 <rudrasenr@gmail.com>
2024-03-16 17:32:46 +0600

    login page added

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatBoxController;
 
+Route::get('/login', [LoginController::class, 'login']);
 Route::get('/chat-box', [ChatBoxController::class, 'chatBox']);
 
 
