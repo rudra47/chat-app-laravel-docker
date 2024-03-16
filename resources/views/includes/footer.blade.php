854dcad040ca773acb0f342d257d8db7452d03ba
Rudra Sen <rudrasenr@gmail.com>
2024-03-16 17:35:18 +0600

    Revert "Design/chat box page setup"

--- resources/views/includes/footer.blade.php
+++
@@ -1,6 +0,0 @@
-
-    </body>
-    <footer>
-        <script src="{{asset('js/app.js')}}"></script>
-    </footer>
-</html>
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChatBoxController;
-
-Route::get('/chat-box', [ChatBoxController::class, 'chatBox']);
-
-
-// Route::get('/', 'WebController@home')->name('home');
